49c3f8ba2167b98fb04008f4b69540476c337bcb
Wrdn28 <ifloraxd@gmail.com>
2024-09-07 01:58:46 +0000

    ginkgo: Set TARGET_BOARD_PLATFORM in device.mk
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -370,6 +370,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Platform
+TARGET_BOARD_PLATFORM := trinket
+TRINKET := trinket
+
+
 # QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
