1911307a042d1d621afe338aba5aaa1211a239f7
Wrdn28 <ifloraxd@gmail.com>
2025-03-09 22:33:07 +0000

    ginkgo: Set product API level to 28
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -412,7 +412,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 # Shipping API
-PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Servicetracker
 PRODUCT_PACKAGES += \
